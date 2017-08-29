38984ea0787e465710a6b6fede70b3c64ebc451e
yukimatJP <tbonfukitama110@gmail.com>
2017-08-29 21:42:23 +0900

    Menuのモデルを作成

---
+++ app/src/main/java/jp/naist/ubi_lab/ikoma4919/models/Menu.kt
@@ -0,0 +1,18 @@
+package jp.naist.ubi_lab.ikoma4919.models
+
+/**
+ * メニューのモデル
+ * @author yuki-mat
+ */
+object Menu {
+    var stapleName: String = ""
+    var mainDishName: String = ""
+    var sideDishName: String = ""
+    var soupName: String = ""
+    var drinkName: String = ""
+    var dessertName: String = ""
+    var energy: String = ""
+    var point0: String = ""
+    var point1: String = ""
+    var point2: String = ""
+}
