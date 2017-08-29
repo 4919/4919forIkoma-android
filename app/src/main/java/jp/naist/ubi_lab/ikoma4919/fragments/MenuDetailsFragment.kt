1802a2c04f5b945c2d00ec2f565405190e9f1e87
yukimatJP <tbonfukitama110@gmail.com>
2017-08-29 18:25:14 +0900

    energy とか、footer部分のUIを作成

--- app/src/main/java/jp/naist/ubi_lab/ikoma4919/fragments/MenuDetailsFragment.kt
+++ app/src/main/java/jp/naist/ubi_lab/ikoma4919/fragments/MenuDetailsFragment.kt
@@ -26,6 +26,10 @@ class MenuDetailsFragment : DialogFragment() {
     private var tvMenuNameDishes: TextView? = null
     private var tvMenuNameDrink: TextView? = null
     private var tvMenuNameDessert: TextView? = null
+    private var tvMenuEnergy: TextView? = null
+    private var tvMenuPoint0: TextView? = null
+    private var tvMenuPoint1: TextView? = null
+    private var tvMenuPoint2: TextView? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater!!.inflate(R.layout.fragment_menu_details, container, false)
@@ -34,6 +38,10 @@ class MenuDetailsFragment : DialogFragment() {
         tvMenuNameDishes = v.findViewById(R.id.tv_menuName_dishes)
         tvMenuNameDrink = v.findViewById(R.id.tv_menuName_drink)
         tvMenuNameDessert = v.findViewById(R.id.tv_menuName_dessert)
+        tvMenuEnergy = v.findViewById(R.id.tv_menuEnergy)
+        tvMenuPoint0 = v.findViewById(R.id.tv_menuPoint_0)
+        tvMenuPoint1 = v.findViewById(R.id.tv_menuPoint_1)
+        tvMenuPoint2 = v.findViewById(R.id.tv_menuPoint_2)
 
         return v
     }
@@ -70,6 +78,7 @@ class MenuDetailsFragment : DialogFragment() {
                     tvMenuNameDishes?.text = "$mainDishName\n$sideDishName\n$soupName"
                     tvMenuNameDrink?.text = drinkName
                     tvMenuNameDessert?.text = dessertName
+                    tvMenuEnergy?.text = "$energy kcal"
 
                 }
             }
@@ -84,6 +93,6 @@ class MenuDetailsFragment : DialogFragment() {
     }
 
     private fun getMenuItemName(dataSnapshot: DataSnapshot, category: String): String? =
-            dataSnapshot.child("menu_list").child(category).child("name")?.getValue(String::class.java)
+            dataSnapshot.child("menu_list")?.child(category)?.child("name")?.getValue(String::class.java)
 
 }
