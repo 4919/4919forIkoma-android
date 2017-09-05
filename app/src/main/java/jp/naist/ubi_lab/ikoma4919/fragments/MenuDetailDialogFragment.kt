307ccb682f449a85f36c6e373531f8a125376f40
yukimatJP <tbonfukitama110@gmail.com>
2017-09-05 22:15:57 +0900

    ダイアログを閉じるボタンを設定

--- app/src/main/java/jp/naist/ubi_lab/ikoma4919/fragments/MenuDetailDialogFragment.kt
+++ app/src/main/java/jp/naist/ubi_lab/ikoma4919/fragments/MenuDetailDialogFragment.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import jp.naist.ubi_lab.ikoma4919.R
 import jp.naist.ubi_lab.ikoma4919.adapters.AllergenGridAdapter
@@ -21,7 +22,7 @@ import java.util.*
  * １日の詳細メニュー の Fragment
  * @author yuki-mat
  */
-class MenuDetailDialogFragment(): DialogFragment(), FireBaseHelper.FireBaseEventListener {
+class MenuDetailDialogFragment(): DialogFragment(), FireBaseHelper.FireBaseEventListener, View.OnClickListener {
     private val TAG = "MenuDetailDialogFragment"
 
     private var fireBaseHelper: FireBaseHelper? = null
@@ -32,6 +33,7 @@ class MenuDetailDialogFragment(): DialogFragment(), FireBaseHelper.FireBaseEvent
     private var tvMenuNameSelected: TextView? = null
     private var rvAllergenList: RecyclerView? = null
     private var allergenGridAdapter: AllergenGridAdapter? = null
+    private var ivBtnDialogClose: ImageView? = null
 
     constructor(category: MenuCategory, date: Date): this() {
         this.category = category
@@ -53,6 +55,9 @@ class MenuDetailDialogFragment(): DialogFragment(), FireBaseHelper.FireBaseEvent
         rvAllergenList?.layoutManager = GridLayoutManager(context, 4)
         rvAllergenList?.adapter = allergenGridAdapter
 
+        ivBtnDialogClose = v.findViewById(R.id.iv_dialog_close)
+        ivBtnDialogClose?.setOnClickListener(this)
+
         return v
     }
 
@@ -65,6 +70,12 @@ class MenuDetailDialogFragment(): DialogFragment(), FireBaseHelper.FireBaseEvent
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.iv_dialog_close -> { closeDialog() }
+        }
     }
 
     override fun onSummaryFetched(menuSummary: MenuSummary) {
@@ -89,5 +100,12 @@ class MenuDetailDialogFragment(): DialogFragment(), FireBaseHelper.FireBaseEvent
 
     }
 
+    fun closeDialog() {
+        fragmentManager
+                .beginTransaction()
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out, android.R.anim.fade_in, android.R.anim.fade_out)
+                .remove(this)
+                .commit()
+    }
 
 }
